bf9a369b4340e71658872a9be7907b39c87171f9
bioDanCho <daniel.cho@thebioagency.com>
2022-12-15 02:02:11 -0500

    side bar collapsed state

--- components/organisms/SideBar.jsx
+++ components/organisms/SideBar.jsx
@@ -4,6 +4,8 @@ import HoverableIcon from '../atoms/HoverableIcon';
 import { BsArrowBarLeft } from 'react-icons/bs';
 import { BsArrowBarRight } from 'react-icons/bs';
 import { BsArrowDownUp } from 'react-icons/bs';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { SlCamrecorder } from 'react-icons/sl';
 import ChannelListItem from '../molecules/ChannelListItem';
 import followedStreamsData from '../../assets/data/followed-streams.json';
 
@@ -42,12 +44,22 @@ const SideBar = () => {
                 </div>
             </div>
 
-            {isOpen && (
+            {isOpen ? (
                 <div className={styles.followed_channels_wrapper}>
                     <div className={styles.sidebar_heading}>
                         Followed channels
                     </div>
                     <BsArrowDownUp color={iconColor} size={16} />
+                </div>
+            ) : (
+                <div className={styles.collapsed_icon_wrapper}>
+                    <HoverableIcon
+                        icon={
+                            <AiOutlineHeart color={iconColor} size={iconSize} />
+                        }
+                        toolTipText={'Followed Channels'}
+                        toolTipPosition={'right'}
+                    />
                 </div>
             )}
 
@@ -64,11 +76,21 @@ const SideBar = () => {
                 })}
             </div>
 
-            {isOpen && (
+            {isOpen ? (
                 <div className={styles.recommended_channels_wrapper}>
                     <div className={styles.sidebar_heading}>
                         recommended channels
                     </div>
+                </div>
+            ) : (
+                <div className={styles.collapsed_icon_wrapper}>
+                    <HoverableIcon
+                        icon={
+                            <SlCamrecorder color={iconColor} size={iconSize} />
+                        }
+                        toolTipText={'Recommended Channels'}
+                        toolTipPosition={'right'}
+                    />
                 </div>
             )}
 
