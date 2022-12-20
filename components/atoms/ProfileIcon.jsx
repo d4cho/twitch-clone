3a20d45f71e843c2fb5df73f40b2a9b8572ffa0e
bioDanCho <daniel.cho@thebioagency.com>
2022-12-20 10:13:36 -0500

    add profile avatar to stream card

--- components/atoms/ProfileIcon.jsx
+++ components/atoms/ProfileIcon.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../../styles/ProfileIcon.module.css';
-import { RxPerson } from 'react-icons/rx';
 
 const ProfileIcon = ({ image }) => {
     const defaultImage = '/default-profile-icon.png';
--- components/molecules/StreamCard.jsx
+++ components/molecules/StreamCard.jsx
@@ -5,6 +5,7 @@ import HoverableIcon from '../atoms/HoverableIcon';
 import HoverableText from '../atoms/HoverableText';
 import Chip from '../atoms/Chip';
 import { nFormatter } from '../../utils/functions';
+import ProfileIcon from '../atoms/ProfileIcon';
 
 const StreamCard = ({ cardData, bgColor }) => {
     const { title, thumbnail_url, user_name, game_name, viewer_count, tags } =
@@ -43,48 +44,55 @@ const StreamCard = ({ cardData, bgColor }) => {
                     </div>
                 </div>
 
-                <div className={styles.title_wrapper}>
-                    <div className={styles.title}>
+                <div className={styles.info_wrapper}>
+                    <div className={styles.avatar}>
+                        <ProfileIcon />
+                    </div>
+                    <div className={styles.info}>
+                        <div className={styles.title}>
+                            <HoverableText
+                                text={title}
+                                fontSize={'14px'}
+                                fontWeight={'bold'}
+                                hoverEffects={{
+                                    changeColor: true,
+                                    addUnderline: false,
+                                }}
+                                initialColor={'#efeff1'}
+                                hoverColor={'#a970ff'}
+                                handleOnClick={() => {}}
+                            />
+                        </div>
+                        <div className={styles.user_name}>{user_name}</div>
                         <HoverableText
-                            text={title}
-                            fontSize={'14px'}
-                            fontWeight={'bold'}
+                            text={game_name}
+                            fontSize={'13px'}
                             hoverEffects={{
                                 changeColor: true,
                                 addUnderline: false,
                             }}
-                            initialColor={'#efeff1'}
+                            initialColor={'#adadb8'}
                             hoverColor={'#a970ff'}
                             handleOnClick={() => {}}
                         />
+                        <div className={styles.tags_wrapper}>
+                            {tags &&
+                                tags.map((tag, idx) => {
+                                    return (
+                                        <React.Fragment key={idx}>
+                                            <Chip chipText={tag} />
+                                        </React.Fragment>
+                                    );
+                                })}
+                        </div>
                     </div>
-                    <HoverableIcon
-                        icon={<SlOptionsVertical />}
-                        toolTipText={'Options'}
-                        toolTipPosition={'bottom'}
-                    />
-                </div>
-                <div className={styles.user_name}>{user_name}</div>
-                <HoverableText
-                    text={game_name}
-                    fontSize={'13px'}
-                    hoverEffects={{
-                        changeColor: true,
-                        addUnderline: false,
-                    }}
-                    initialColor={'#adadb8'}
-                    hoverColor={'#a970ff'}
-                    handleOnClick={() => {}}
-                />
-                <div className={styles.tags_wrapper}>
-                    {tags &&
-                        tags.map((tag, idx) => {
-                            return (
-                                <React.Fragment key={idx}>
-                                    <Chip chipText={tag} />
-                                </React.Fragment>
-                            );
-                        })}
+                    <div>
+                        <HoverableIcon
+                            icon={<SlOptionsVertical size={20} />}
+                            toolTipText={'Options'}
+                            toolTipPosition={'bottom'}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
