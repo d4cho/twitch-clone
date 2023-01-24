71758ea8e9ab7c512dd7d08238185f03b166e7cb
bioDanCho <daniel.cho@thebioagency.com>
2023-01-24 17:37:46 -0500

    update prod server url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
     ? 'http://localhost:3000'
-    : 'https://twitch-clone-gamma.vercel.app';
+    : 'https://twitch-clone-tau.vercel.app';
