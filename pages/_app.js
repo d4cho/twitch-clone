6b2fc5f6c12a8e401a627d0cd7baddf355123b19
d4cho <danielcho91@gmail.com>
2023-11-23 16:53:47 -0500

    add bio widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
-import Layout from '../components/layouts/Layout';
-import { AppContextProvider } from '../context/AppContext';
-import '../styles/globals.css';
+import Layout from "../components/layouts/Layout";
+import { AppContextProvider } from "../context/AppContext";
+import "../styles/globals.css";
+import BioStorybookWidget from "bio-storybook-widget";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <AppContextProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </AppContextProvider>
-    );
+  return (
+    <AppContextProvider>
+      <BioStorybookWidget projectSource={"twitch"} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContextProvider>
+  );
 }
 
 export default MyApp;
